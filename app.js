da714f520ca6435bfb35199d91712ab585e7240f
a00783576 <arturo-gutierrez@live.com>
2016-04-04 18:27:56 -0500

    Initial commit.

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var app = express();
+var fortunes = [
+"Test 1",
+"Test 2",
+"Test 3",
+"Test 4",
+"Test 5"
+];
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/about', function(req, res){
+	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', {fortune: randomFortune});
+});
+
+app.use(function(req, res, next){
+	res.status(404);
+	res.send('404');
+});
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.send('500');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:' 
+	+ app.get('port') + '; press Ctrl-C to terminate.');
+})
